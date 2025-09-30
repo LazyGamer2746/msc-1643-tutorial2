37ed5b8dc59043a9ca050e270140833bec8e8669
LazyGamer2746 <jstratto1@ltu.edu>
2025-09-30 16:07:16 -0400

    Pong Game

--- Assets/_Scripts/Ball.cs
+++ Assets/_Scripts/Ball.cs
@@ -4,19 +4,34 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public AudioClip bounceSound;
+    public float speed = 36.0f;
     private Rigidbody rb;
 
     // Start is called before the first frame update
     void Start()
     {
+        transform.eulerAngles = new Vector3(0, 90, 0);
         rb = GetComponent<Rigidbody>();
 
-        rb.velocity = new Vector3(-30.0f, 0f, 10.0f);
+        float angle = Random.value * 44 - 22;
+
+        if (Random.value < .5f)
+        {
+            angle += 180;
+        }
+
+        transform.eulerAngles += new Vector3(0, angle, 0);
+
+        rb.velocity = transform.forward * speed;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision colosion)
     {
+        if (!colosion.transform.CompareTag("Floor"))
+        {
+            AudioSource.PlayClipAtPoint(bounceSound, transform.position, 1.0f);
+        }
         
     }
 }
--- Assets/_Scripts/DeathZone.cs
+++ Assets/_Scripts/DeathZone.cs
@@ -7,12 +7,14 @@ public class DeathZone : MonoBehaviour
     public Transform startPos;
     public GameObject ballPreFab;
     public int opposingPlayer;
+    public AudioClip outSound;
 
     private void OnTriggerEnter(Collider other)
     
     {
         if (other.transform.CompareTag("Ball"))
         {
+            AudioSource.PlayClipAtPoint(outSound, other.transform.position, 1.0f);
             //Debug.Log($"{other.transform.name} just entered {transform.name}");
             GameManager.AddScore(opposingPlayer);
 
